38c4abb6f2053f369c5873fc3e2c874d9a9a55f5
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-14 22:50:03 -0600

    [add] routes parametros

--- LaravelCurso/app/Http/routes.php
+++ LaravelCurso/app/Http/routes.php
@@ -14,6 +14,14 @@ Route::get('prueba',function(){
 	return 'Hola desde routes.php';
 });
 
+route::get('nombre/{nombre}',function($nombre){
+	return 'mi nombre es: '.$nombre;
+});
+
+route::get('edad/{edad}',function($edad){
+	return 'mi edad es: '.$edad;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
