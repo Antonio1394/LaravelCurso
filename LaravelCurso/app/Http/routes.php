025cf34fcbed3767ef0d6b4878d62532ae17af7d
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-14 22:45:37 -0600

    [add] routes

--- LaravelCurso/app/Http/routes.php
+++ LaravelCurso/app/Http/routes.php
@@ -10,6 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('prueba',function(){
+	return 'Hola desde routes.php';
+});
 
 Route::get('/', function () {
     return view('welcome');
