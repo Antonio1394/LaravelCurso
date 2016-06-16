31a8693a377607a27596c2d699d007607b4fbf38
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-15 23:53:16 -0600

    [add] frontController

---
+++ LaravelCurso/app/Http/Controllers/FrontController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class FrontController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function contacto()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function reviews()
+    {
+        //
+    }
+
+    
+}
--- LaravelCurso/app/Http/routes.php
+++ LaravelCurso/app/Http/routes.php
@@ -11,25 +11,7 @@
 |
 */
 
-Route::get('controlador','PruebaController@index');
-Route::get('name/{nombre}','PruebaController@nombre');
-Route::resource('movie','MovieController');
-Route::get('prueba',function(){
-	return 'Hola desde routes.php';
-});
+Route::get('/','FrontController@index');
+Route::get('contacto','FrontController@contacto');
+Route::get('reviews','FrontController@reviews');
 
-route::get('nombre/{nombre}',function($nombre){
-	return 'mi nombre es: '.$nombre;
-});
-
-route::get('edad/{edad}',function($edad){
-	return 'mi edad es: '.$edad;
-});
-
-route::get('edad2/{edad?}',function($edad=20){
-	return 'mi edad es: '.$edad;
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
