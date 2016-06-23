1669319ef7ac4e10d65cbd3380dc2655bca2e8e2
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-22 22:47:15 -0600

    [update] fix field public

---
+++ LaravelCurso/public/js/script.js
@@ -0,0 +1,23 @@
+$('#registro').click(function(){
+    var dato = $("#genre").val();
+    var route = '/genero';
+    var token = $('#token').val();
+
+    $.ajax({
+        url: route,
+        headers: {'X-CSRF-TOKEN': token},
+        type: 'POST',
+        dataType: 'json',
+        data: {genre: dato},
+        success: function(res)
+        {
+            $("#msj-succ").html(res.mensaje);
+            $("#msj-success").fadeIn();
+        },
+        error: function(msj){
+            console.log(msj.responseJSON.genre);
+            $("#msj").html(msj.responseJSON.genre);
+            $("#msj-error").fadeIn();
+        }
+    });
+});
---
+++ LaravelCurso/public/js/script2.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    Cargar();
+
+});
+
+function Cargar(){
+    var tablaDatos = $('#datos');
+    var route = '/generos';
+
+    $('#datos').empty();
+
+    $.get(route, function(res){
+
+        if (res.success) {
+            $(res.data).each(function(key, value){
+                htmlContent = '<tr><td>' + value.genre + "</td><td>";
+                htmlContent += "<button value='" + value.id + "' OnClick='Mostrar(this);' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Editar</button>";
+                htmlContent += "<button value='" + value.id + "' OnClick='Eliminar(this);' class='btn btn-danger'>Eliminar</button></td></tr>";
+                tablaDatos.append(htmlContent);
+            });
+        } else {
+            alert('error');
+        }
+
+    });
+}
+
+function Mostrar(btn){
+    console.log(btn.value);
+    var route = "/genero/" + btn.value + "/edit";
+
+    $.get(route, function(res){
+        if (res.success) {
+            $("#genre").val(res.data.genre);
+            $("#id").val(res.data.id);
+        } else {
+            alert('error');
+        }
+
+    });
+}
+
+
+function Eliminar(btn){
+    var route = "/genero/" + btn.value;
+    var token = $("#token").val();
+
+    $.ajax({
+        url: route,
+        headers: {'X-CSRF-TOKEN': token},
+        type: 'DELETE',
+        dataType: 'json',
+        success: function(res){
+            Cargar();
+            $("#msj-succ").html(res.mensaje);
+            $("#msj-success").fadeIn();
+        }
+    });
+}
+
+$("#actualizar").click(function(){
+    var value = $("#id").val();
+    var dato = $("#genre").val();
+    var route = "/genero/" + value;
+    var token = $("#token").val();
+
+    $.ajax({
+        url: route,
+        headers: {'X-CSRF-TOKEN': token},
+        type: 'PUT',
+        dataType: 'json',
+        data: {genre: dato},
+        success: function(res){
+            Cargar();
+            $('#myModal').modal('toggle');
+            $("#msj-succ").html(res.mensaje);
+            $("#msj-success").fadeIn();
+        }
+    });
+
+});
