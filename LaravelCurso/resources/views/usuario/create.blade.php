75decb4f8360f3f6702aac44717630788560ede8
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-22 23:01:16 -0600

    [add] form users

--- LaravelCurso/resources/views/usuario/create.blade.php
+++ LaravelCurso/resources/views/usuario/create.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-	<h1>	hola</h1>
+<br>
+	{!! Form::open()!!}
+		<div class="form-group">
+			{!! Form::label('Nombre: ')!!}
+			{!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingresa el nombre del usuario'])!!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('Correo: ')!!}
+			{!! Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingresa el Correo del usuario'])!!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('Password: ')!!}
+			{!! Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa el Password del usuario'])!!}
+		</div>
+		{!! Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+	{!! Form::close()!!}
 @stop
