87597b188208c2aed23fba0866a74d1cf73004a1
Antonio1394 <antoniorivm1394@gmail.com>
2016-06-23 23:59:12 -0600

    []

--- LaravelCurso/resources/views/usuario/create.blade.php
+++ LaravelCurso/resources/views/usuario/create.blade.php
@@ -3,18 +3,7 @@
 @section('content')
 <br>
 	{!! Form::open(['route'=>'usuario.store','method'=>'POST'])!!}
-		<div class="form-group">
-			{!! Form::label('Nombre: ')!!}
-			{!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingresa el nombre del usuario'])!!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('Correo: ')!!}
-			{!! Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingresa el Correo del usuario'])!!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('Password: ')!!}
-			{!! Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa el Password del usuario'])!!}
-		</div>
+		@include('usuario.forms.usr')
 		{!! Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
 	{!! Form::close()!!}
 @stop
--- LaravelCurso/resources/views/usuario/edit.blade.php
+++ LaravelCurso/resources/views/usuario/edit.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.admin')
 @section('content')
 {!! Form::model($user,['route'=>['usuario.update',$user->id],'method'=>'PUT'])!!}
-		<div class="form-group">
-			{!! Form::label('Nombre: ')!!}
-			{!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingresa el nombre del usuario'])!!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('Correo: ')!!}
-			{!! Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingresa el Correo del usuario'])!!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('Password: ')!!}
-			{!! Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa el Password del usuario'])!!}
-		</div>
+		@include('usuario.forms.usr')
 		{!! Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
 	{!! Form::close()!!}
 @stop
