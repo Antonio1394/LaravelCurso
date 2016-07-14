84027a2b73c5be9264159af46bab3d6de4f0abdb
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-14 00:11:44 -0600

    [add] js

--- LaravelCurso/resources/views/genero/create.blade.php
+++ LaravelCurso/resources/views/genero/create.blade.php
@@ -2,5 +2,6 @@
 @section('content')
 	{!! Form::open() !!}
 		@include('genero.form.genero')
+		{!! link_to('#',$title='Registrar',$attributes=['id'=>'registro','class'=>'btn btn-primary'],$secure=null)!!}
 	{!! Form::close()!!}
 @endsection
