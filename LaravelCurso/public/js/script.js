3b86d216355cd5556345c43e99958b6946732970
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-16 22:30:14 -0600

    [update] token js

--- LaravelCurso/public/js/script.js
+++ LaravelCurso/public/js/script.js
@@ -1,9 +1,10 @@
 $('#registro').click(function(){
     var dato= $('#genre').val();
     var route="http://cinema.app/genero";
-
+    var token= $("#token").val();
     $.ajax({
         url:        route,
+        headers:    {'X-CSRF-TOKEN':token},
         type:       'POST',
         dataType:   'json',
         data:{genre:dato }
--- LaravelCurso/resources/views/genero/create.blade.php
+++ LaravelCurso/resources/views/genero/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
 	{!! Form::open() !!}
-		<input type="hidden" name="_token" value="{{ crsf_toke() }}" id="toke">
+		<input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">
 		@include('genero.form.genero')
 		{!! link_to('#',$title='Registrar',$attributes=['id'=>'registro','class'=>'btn btn-primary'],$secure=null)!!}
 	{!! Form::close()!!}
