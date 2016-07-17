97d6e1ec1c1d6d580ff0a85606e307ad0c7edc5d
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-16 22:50:40 -0600

    [add] message success

--- LaravelCurso/resources/views/genero/create.blade.php
+++ LaravelCurso/resources/views/genero/create.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.admin')
 @section('content')
 	{!! Form::open() !!}
+		<div id="msj-success" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+  		<strong>Genero Creado Exitosamente</strong>
+		</div>
 		<input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">
 		@include('genero.form.genero')
 		{!! link_to('#',$title='Registrar',$attributes=['id'=>'registro','class'=>'btn btn-primary'],$secure=null)!!}
