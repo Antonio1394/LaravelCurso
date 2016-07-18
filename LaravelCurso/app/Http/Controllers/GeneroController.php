63a787d987b96dc03d1f8d1b11673e2ef4add00f
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-17 22:53:08 -0600

    [add] view index genero

--- LaravelCurso/app/Http/Controllers/GeneroController.php
+++ LaravelCurso/app/Http/Controllers/GeneroController.php
@@ -16,7 +16,7 @@ class GeneroController extends Controller
      */
     public function index()
     {
-        //
+        return view('genero.index');
     }
 
     /**
---
+++ LaravelCurso/resources/views/genero/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+	@section('content')
+	<table class="table">
+		<thead>
+			<th>Nombre</th>
+			<th>Operaciones</th>
+		</thead>
+		<tbody id="datos"></tbody>
+	</table>
+	@endsection
