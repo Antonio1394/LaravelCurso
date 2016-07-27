acfbadc4533c54e4b2f758b7f551927989f0d139
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-26 22:57:07 -0600

    [add] patials users

--- LaravelCurso/resources/views/usuario/index.blade.php
+++ LaravelCurso/resources/views/usuario/index.blade.php
@@ -3,23 +3,26 @@
 @include('alerts.success')
 
 @section('content')
-	<table class="table">
-		<thead>
-			<th>Nombre</th>
-			<th>Email</th>
-			<th>Operaciones</th>
-		</thead>
-		@foreach($users as $user)
-		<tbody>
-			<td>{{$user->name}}</td>
-			<td>{{$user->email}}</td>
-			<td>
-				{!!link_to_route('usuario.edit', $title = 'Editar', $parameters = $user->id, $attributes = ['class'=>'btn btn-default'])!!}
-			</td>
-		</tbody>
-		@endforeach
-	</table>
-	{!! $users->render() !!}
+	<div class="users">
+		<table class="table">
+			<thead>
+				<th>Nombre</th>
+				<th>Email</th>
+				<th>Operaciones</th>
+			</thead>
+			@foreach($users as $user)
+			<tbody>
+				<td>{{$user->name}}</td>
+				<td>{{$user->email}}</td>
+				<td>
+					{!!link_to_route('usuario.edit', $title = 'Editar', $parameters = $user->id, $attributes = ['class'=>'btn btn-default'])!!}
+				</td>
+			</tbody>
+			@endforeach
+		</table>
+	{!! $users->render() !!}	
+	</div>
+	
 @endsection
 @section('scripts')
 	    {!!Html::script('js/script3.js')!!}
