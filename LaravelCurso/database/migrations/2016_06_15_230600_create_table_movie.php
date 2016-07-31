31e37e69017cf31c665f57fcba5acc740b076cdb
Antonio1394 <antoniorivm1394@gmail.com>
2016-07-30 22:43:25 -0600

    [update] fix migration

--- LaravelCurso/database/migrations/2016_06_15_230600_create_table_movie.php
+++ LaravelCurso/database/migrations/2016_06_15_230600_create_table_movie.php
@@ -14,8 +14,8 @@ class CreateTableMovie extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->increments('id');
-             $table->string('name');
-             $table->string('path');
+            $table->string('name');
+            $table->string('path');
             $table->string('cast');
             $table->string('direction');
             $table->string('duration');
