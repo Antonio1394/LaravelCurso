5155ce9b119118599dbefc081e6dab690c281e91
Antonio1394 <antoniorivm1394@gmail.com>
2016-08-01 23:18:41 -0600

    [add] function movie

--- LaravelCurso/app/Movie.php
+++ LaravelCurso/app/Movie.php
@@ -12,8 +12,15 @@ class Movie extends Model
 
     public function setPathAttribute($path){
     	$this->attributes['path']=Carbon::now()->second.$path->getClientOriginalName();
-    	$name=Carbon::now()->second.$path->getClientOriginalName();
+    	$name=Carbon::now()->second.$path->getClientOriginalName+();
     	\Storage::disk('local')->put($name, \File::get($path));
+    }
+
+    public static function Movies(){
+    	return DB::table('movies')
+    		->join('genres','genres.id','=','movies.genre_id')
+    		->select('movies.*','genres.genre')
+    		->get();
     }
 
 }
