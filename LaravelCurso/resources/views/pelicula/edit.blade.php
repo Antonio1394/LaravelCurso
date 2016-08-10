1404637a33d51227d299f24fb689452a74353d78
Antonio1394 <antoniorivm1394@gmail.com>
2016-08-09 23:30:47 -0600

    [add] view edit movie

---
+++ LaravelCurso/resources/views/pelicula/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+	@section('content')
+		@include('alerts.request')
+
+		{!! Form::model($movie,['route'=>'pelicula.update',$movie->id],'method'=>'PUT','files'=>true!!}
+			@include('pelicula.forms.pelicula')
+			{!! Form::submit('Actualizar',['class','btn btn-primary'])!!}
+			{!!Form::close()!!}
+			{!! Form::open(['route'=>'[pelicula.update',$movie->id,'method'=>'PUT']!!}
+			{!! Form::submit('Eliminar',['class','btn btn-danger'])!!}
+			{!!Form::close()!!}
+	@endsection
+
+
