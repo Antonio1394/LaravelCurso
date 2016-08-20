2b1de68c2c1aa0eb1ab263218a84c371c1afd9c6
Antonio1394 <antoniorivm1394@gmail.com>
2016-08-19 23:08:05 -0600

    [update] fix view edit movie

--- LaravelCurso/resources/views/pelicula/edit.blade.php
+++ LaravelCurso/resources/views/pelicula/edit.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.admin')
-	@section('content')
-		@include('alerts.request')
 
-		{!! Form::model($movie,['route'=>'pelicula.update',$movie->id],'method'=>'PUT','files'=>true!!}
-			@include('pelicula.forms.pelicula')
-			{!! Form::submit('Actualizar',['class','btn btn-primary'])!!}
-			{!!Form::close()!!}
-			
-			{!! Form::open(['route'=>'[pelicula.update',$movie->id,'method'=>'PUT']!!}
-			{!! Form::submit('Eliminar',['class','btn btn-danger'])!!}
-			{!!Form::close()!!}
-	@endsection
+    @section('content')
+        @include('alerts.request')
 
+        {!!Form::model($movie, ['route' => ['pelicula.update', $movie->id], 'method' => 'PUT', 'files' => true])!!}
 
+            @include('pelicula.forms.pelicula')
+
+            {!!Form::submit('Actualizar', ['class' => 'btn btn-primary'])!!}
+        {!!Form::close()!!}
+
+        {!!Form::open(['route' => ['pelicula.destroy', $movie->id], 'method' => 'DELETE'])!!}
+            {!!Form::submit('Eliminar', ['class' => 'btn btn-danger'])!!}
+        {!!Form::close()!!}
+
+    @endsection
